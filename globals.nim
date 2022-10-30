3eafbce6dfe82c7d1493607a99d8a27ca3a85764
MrPerux <perschrijver@gmail.com>
2022-10-30 16:02:06 +0100

    Move UI types to globals.

--- globals.nim
+++ globals.nim
@@ -1,5 +1,37 @@
+import std/options
+
+{.experimental: "codeReordering".}
+
+# every object (position) has a global position
+# every (non-root) object (position) has a parent object with a layout algorithm object (layouter)
+# some objects (shape) have an interactable arbitrary counterpart object (data)
+
+### 2D position object
+type Pos* = object
+    x*: cint
+    y*: cint
+
+proc `+`*(a: Pos, b: Pos): Pos =
+    Pos(x: a.x + b.x, y: a.y + b.y)
+proc `-`*(a: Pos, b: Pos): Pos =
+    Pos(x: a.x - b.x, y: a.y - b.y)
+
+
+### Base objects for UI elements
+type UIChild* = object
+    relative_pos*: Pos
+    is_float*: bool
+    is_visible_or_interactable*: bool
+    itself*: UIObject
+    sibling_index*: cint
+
+type UIObject* = ref object of RootObj
+    children*: seq[UIChild]
+    parent*: Option[UIObject]
+    size*: Pos
 
 type Globals* = object
   running*: bool
   width*: cint
   height*: cint
+
--- ui_objects.nim
+++ ui_objects.nim
@@ -4,37 +4,6 @@ import sdl2/image
 
 import globals
 
-{.experimental: "codeReordering".}
-
-# every object (position) has a global position
-# every (non-root) object (position) has a parent object with a layout algorithm object (layouter)
-# some objects (shape) have an interactable arbitrary counterpart object (data)
-
-
-### 2D position object
-type Pos* = object
-    x*: cint
-    y*: cint
-
-proc `+`*(a: Pos, b: Pos): Pos =
-    Pos(x: a.x + b.x, y: a.y + b.y)
-proc `-`*(a: Pos, b: Pos): Pos =
-    Pos(x: a.x - b.x, y: a.y - b.y)
-
-
-### Base objects for UI elements
-type UIChild = object
-    relative_pos: Pos
-    is_float: bool
-    is_visible_or_interactable: bool
-    itself: UIObject
-    sibling_index: cint
-
-type UIObject = ref object of RootObj
-    children: seq[UIChild]
-    parent: Option[UIObject]
-    size: Pos
-
 method draw*(obj: UIObject, globals: Globals, position: Pos, renderer: RendererPtr) {.base.} = discard
 method onClick*(obj: UIObject): bool {.base.} = discard
 
@@ -83,7 +52,7 @@ method draw*(obj: MySidebar, globals: Globals, position: Pos, renderer: Renderer
         i += 1
     
 method onClick*(obj: MySidebar): bool =
-    discard
+    echo "CLICKED SIDEBAR"
 
 
 ## Root UI element
