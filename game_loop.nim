5539e7da181d2c573339aa65819c99ff8f22128c
MrPerux <perschrijver@gmail.com>
2022-10-30 17:00:09 +0100

    Switch to 4 spaces everywhere.

--- game_loop.nim
+++ game_loop.nim
@@ -15,116 +15,116 @@ import globals
 import os
 
 proc draw(globals: Globals, renderer: RendererPtr, font: FontPtr, dt: float32) =
-  # Background
-  renderer.setDrawColor 8, 21, 27, 255 # dark cyaan
-  renderer.clear()
+    # Background
+    renderer.setDrawColor 8, 21, 27, 255 # dark cyaan
+    renderer.clear()
 
 
 proc handleInput(globals: var Globals, input: Input) =
-  if input.kind == None:
-    return
-  if input.kind == InputKind.Keydown and input.is_ascii == false and input.mod_ctrl and input.scancode == Scancode.SDL_SCANCODE_C:
-    globals.running = false
-  echo $input
+    if input.kind == None:
+        return
+    if input.kind == InputKind.Keydown and input.is_ascii == false and input.mod_ctrl and input.scancode == Scancode.SDL_SCANCODE_C:
+        globals.running = false
+    echo $input
 
 
 proc main =
-  let WIDTH: cint = if existsEnv("WSL_INTEROP"): 2560 else: 1920
-  let HEIGHT: cint = if existsEnv("WSL_INTEROP"): 1440 else: 1023
-  var globals = Globals(running: true, width: WIDTH, height: HEIGHT)
+    let WIDTH: cint = if existsEnv("WSL_INTEROP"): 2560 else: 1920
+    let HEIGHT: cint = if existsEnv("WSL_INTEROP"): 1440 else: 1023
+    var globals = Globals(running: true, width: WIDTH, height: HEIGHT)
 
-  # SDL Stuff
-  sdlFailIf(not sdl2.init(INIT_VIDEO or INIT_TIMER or INIT_EVENTS)):
-    "SDL2 initialization failed"
-  defer: sdl2.quit()
+    # SDL Stuff
+    sdlFailIf(not sdl2.init(INIT_VIDEO or INIT_TIMER or INIT_EVENTS)):
+        "SDL2 initialization failed"
+    defer: sdl2.quit()
 
-  let window = createWindow(
-    title = "Gebruik de pijltjes",
-    x = SDL_WINDOWPOS_CENTERED,
-    y = SDL_WINDOWPOS_CENTERED,
-    w = globals.width,
-    h = globals.height,
-    flags = SDL_WINDOW_SHOWN or SDL_WINDOW_MAXIMIZED or SDL_WINDOW_BORDERLESS or SDL_WINDOW_RESIZABLE
-  )
+    let window = createWindow(
+        title = "Gebruik de pijltjes",
+        x = SDL_WINDOWPOS_CENTERED,
+        y = SDL_WINDOWPOS_CENTERED,
+        w = globals.width,
+        h = globals.height,
+        flags = SDL_WINDOW_SHOWN or SDL_WINDOW_MAXIMIZED or SDL_WINDOW_BORDERLESS or SDL_WINDOW_RESIZABLE
+    )
 
-  sdlFailIf window.isNil: "window could not be created"
-  defer: window.destroy()
+    sdlFailIf window.isNil: "window could not be created"
+    defer: window.destroy()
 
-  let renderer = createRenderer(
-    window = window,
-    index = -1,
-    flags = Renderer_Accelerated or Renderer_PresentVsync or Renderer_TargetTexture
-  )
-  sdlFailIf renderer.isNil: "renderer could not be created"
-  defer: renderer.destroy()
+    let renderer = createRenderer(
+        window = window,
+        index = -1,
+        flags = Renderer_Accelerated or Renderer_PresentVsync or Renderer_TargetTexture
+    )
+    sdlFailIf renderer.isNil: "renderer could not be created"
+    defer: renderer.destroy()
 
-  sdlFailIf(not ttfInit()): "SDL_TTF initialization failed"
-  defer: ttfQuit()
+    sdlFailIf(not ttfInit()): "SDL_TTF initialization failed"
+    defer: ttfQuit()
 
-  let myRoot = initMyRoot(globals, renderer)
+    let myRoot = initMyRoot(globals, renderer)
 
-  # Setup font
-  let font = ttf.openFont("Hack Regular Nerd Font Complete.ttf", 16)
-  sdlFailIf font.isNil: "font could not be created"
+    # Setup font
+    let font = ttf.openFont("Hack Regular Nerd Font Complete.ttf", 16)
+    sdlFailIf font.isNil: "font could not be created"
 
-  # Gameloop variables
-  var
-    dt: float32
+    # Gameloop variables
+    var
+        dt: float32
 
-    counter: uint64
-    previousCounter: uint64
+        counter: uint64
+        previousCounter: uint64
 
 
-  # Start gameloop
-  counter = getPerformanceCounter()
-  while globals.running:
-    previousCounter = counter
+    # Start gameloop
     counter = getPerformanceCounter()
+    while globals.running:
+        previousCounter = counter
+        counter = getPerformanceCounter()
 
-    dt = (counter - previousCounter).float / getPerformanceFrequency().float
+        dt = (counter - previousCounter).float / getPerformanceFrequency().float
 
-    var event = defaultEvent
+        var event = defaultEvent
 
-    while pollEvent(event):
-      case event.kind
-      of QuitEvent:
-        globals.running = false
-        break
+        while pollEvent(event):
+            case event.kind
+            of QuitEvent:
+                globals.running = false
+                break
 
-      of TextInput:
-        let c = event.evTextInput.text[0]
-        echo "TextInput"
-        globals.handleInput(toInput(c, getModState()))
+            of TextInput:
+                let c = event.evTextInput.text[0]
+                echo "TextInput"
+                globals.handleInput(toInput(c, getModState()))
 
-      of EventType.KeyDown:
-        echo "Keydown"
-        globals.handleInput(toInput(event.evKeyboard.keysym.scancode, cast[
-            Keymod](event.evKeyboard.keysym.modstate)))
-    
-      of EventType.MouseMotion:
-        var new_hovered: seq[UIObject] = @[]
-        getElementsContaining(new_hovered, myRoot, pos(event.evMouseMotion.x, event.evMouseMotion.y))
-        for obj in new_hovered:
-          if not globals.hovered.contains(obj):
-            obj.is_hovered = true
-            obj.onMouseEnter()
+            of EventType.KeyDown:
+                echo "Keydown"
+                globals.handleInput(toInput(event.evKeyboard.keysym.scancode, cast[
+                        Keymod](event.evKeyboard.keysym.modstate)))
+        
+            of EventType.MouseMotion:
+                var new_hovered: seq[UIObject] = @[]
+                getElementsContaining(new_hovered, myRoot, pos(event.evMouseMotion.x, event.evMouseMotion.y))
+                for obj in new_hovered:
+                    if not globals.hovered.contains(obj):
+                        obj.is_hovered = true
+                        obj.onMouseEnter()
 
+                for obj in globals.hovered:
+                    if not new_hovered.contains(obj):
+                        obj.is_hovered = false
+                        obj.onMouseExit()
+                globals.hovered = new_hovered
+
+            else:
+                discard
+
+        globals.draw(renderer, font, dt)
+        myRoot.draw(globals, pos(0, 0), renderer)
         for obj in globals.hovered:
-          if not new_hovered.contains(obj):
-            obj.is_hovered = false
-            obj.onMouseExit()
-        globals.hovered = new_hovered
-
-      else:
-        discard
-
-    globals.draw(renderer, font, dt)
-    myRoot.draw(globals, pos(0, 0), renderer)
-    for obj in globals.hovered:
-      let pos = getAbsolutePosition(obj)
-      var r = rect(pos.x, pos.y, obj.size.x, obj.size.y)
-      renderer.setDrawColor(255, 0, 255, 255)
-      renderer.drawRect(r)
-    renderer.present()
+            let pos = getAbsolutePosition(obj)
+            var r = rect(pos.x, pos.y, obj.size.x, obj.size.y)
+            renderer.setDrawColor(255, 0, 255, 255)
+            renderer.drawRect(r)
+        renderer.present()
 
 main()
--- sdl_stuff.nim
+++ sdl_stuff.nim
@@ -11,66 +11,66 @@ type SDLException = object of Defect
 
 
 template sdlFailIf*(condition: typed, reason: string) =
-  if condition: raise SDLException.newException(
-    reason & ", SDL error " & $getError()
-  )
+    if condition: raise SDLException.newException(
+        reason & ", SDL error " & $getError()
+    )
 
 
 proc drawText*(renderer: RendererPtr, font: FontPtr, text: cstring,
-    color: Color, x: cint, y: cint) =
-  if text.len == 0:
-    return
-  let
-    surface = ttf.renderTextBlended(font, text, color)
-    texture = renderer.createTextureFromSurface(surface)
+        color: Color, x: cint, y: cint) =
+    if text.len == 0:
+        return
+    let
+        surface = ttf.renderTextBlended(font, text, color)
+        texture = renderer.createTextureFromSurface(surface)
 
-  surface.freeSurface
-  defer: texture.destroy
+    surface.freeSurface
+    defer: texture.destroy
 
-  var r = rect(
-    x,
-    y,
-    surface.w,
-    surface.h
-  )
-  renderer.copy texture, nil, addr r
+    var r = rect(
+        x,
+        y,
+        surface.w,
+        surface.h
+    )
+    renderer.copy texture, nil, addr r
 
 
 type InputKind* = enum
-  Keydown
-  None
+    Keydown
+    None
 
 
 type Input* = object
-  case kind*: InputKind:
-  of Keydown:
-    is_ascii*: bool
-    character*: char
-    scancode*: Scancode
-    mod_shift*: bool
-    mod_ctrl*: bool
-    mod_alt*: bool
-  of None:
-    nil
+    case kind*: InputKind:
+    of Keydown:
+        is_ascii*: bool
+        character*: char
+        scancode*: Scancode
+        mod_shift*: bool
+        mod_ctrl*: bool
+        mod_alt*: bool
+    of None:
+        nil
 
 
 func isDisplayableAsciiCharacterMap(): array[0..127, bool] =
-  for c in " qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#$%^&*()_+~`-=[]\\;',./{}|:\"<>?":
-    result[cast[cint](c)] = true
+    for c in " qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#$%^&*()_+~`-=[]\\;',./{}|:\"<>?":
+        result[cast[cint](c)] = true
 
 
 const
-  MOD_SHIFT = KMOD_LSHIFT or KMOD_RSHIFT
-  MOD_CTRL = KMOD_LCTRL or KMOD_RCTRL
-  MOD_ALT = KMOD_LALT or KMOD_RALT
+    MOD_SHIFT = KMOD_LSHIFT or KMOD_RSHIFT
+    MOD_CTRL = KMOD_LCTRL or KMOD_RCTRL
+    MOD_ALT = KMOD_LALT or KMOD_RALT
 
 func toInput*(c: char, mod_state: Keymod): Input =
-  const table = isDisplayableAsciiCharacterMap()
-  if cast[cint](c) in 0..127 and table[cast[cint](c)]:
-    Input(kind: Keydown, is_ascii: true, character: c, scancode: cast[Scancode](0), mod_shift: bool(mod_state and MOD_SHIFT), mod_ctrl: bool(mod_state and MOD_CTRL), mod_alt: bool(mod_state and MOD_ALT))
-  else:
-    Input(kind: None)
+    const table = isDisplayableAsciiCharacterMap()
+    if cast[cint](c) in 0..127 and table[cast[cint](c)]:
+        Input(kind: Keydown, is_ascii: true, character: c, scancode: cast[Scancode](0), mod_shift: bool(mod_state and MOD_SHIFT), mod_ctrl: bool(mod_state and MOD_CTRL), mod_alt: bool(mod_state and MOD_ALT))
+    else:
+        Input(kind: None)
 
 
 func toInput*(key: Scancode, mod_state: Keymod): Input =
-  Input(kind: Keydown, is_ascii: false, character: cast[char](0), scancode: key, mod_shift: bool(mod_state and MOD_SHIFT), mod_ctrl: bool(mod_state and MOD_CTRL), mod_alt: bool(mod_state and MOD_ALT))
+    Input(kind: Keydown, is_ascii: false, character: cast[char](0), scancode: key, mod_shift: bool(mod_state and MOD_SHIFT), mod_ctrl: bool(mod_state and MOD_CTRL), mod_alt: bool(mod_state and MOD_ALT))
